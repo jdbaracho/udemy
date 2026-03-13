981633eae9b9abed44679e1175fc55cafe30c1d7
jdbaracho <joao.d.baracho@gmail.com>
2026-03-13 17:42:31 +0000

    [roll_dice] custom widgets

---
+++ roll_dice_app/lib/gradient_container.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:roll_dice_app/styled_text.dart';
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          colors: [Colors.deepPurple, Colors.purple],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Center(
+        child: StyledText(),
+      ),
+    );
+  }
+}
--- roll_dice_app/lib/main.dart
+++ roll_dice_app/lib/main.dart
@@ -1,25 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:roll_dice_app/gradient_container.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [Colors.deepPurple, Colors.purple],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: Center(
-            child: Text(
-              'Hello World!',
-              style: TextStyle(color: Colors.white, fontSize: 28),
-            ),
-          ),
-        ),
-      ),
-    ),
-  );
+  runApp(MaterialApp(home: Scaffold(body: GradientContainer())));
 }
