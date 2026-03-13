5f99894436c16a3da6cb3fc3cd2e942a142fd5a5
jdbaracho <joao.d.baracho@gmail.com>
2026-03-13 17:52:28 +0000

    [roll_dice] images

--- roll_dice_app/lib/gradient_container.dart
+++ roll_dice_app/lib/gradient_container.dart
@@ -19,7 +19,7 @@ class GradientContainer extends StatelessWidget {
           end: endAlignment,
         ),
       ),
-      child: Center(child: StyledText('Hello World!')),
+      child: Center(child: Image.asset('assets/images/dice-2.png', width: 200)),
     );
   }
 }
