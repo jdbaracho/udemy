3e614a30c40f71d96e596fba7e12213ad2fe2ded
jdbaracho <joao.d.baracho@gmail.com>
2026-03-16 16:42:50 +0000

    [quiz_app] column

--- quiz_app/lib/questions_screen.dart
+++ quiz_app/lib/questions_screen.dart
@@ -10,6 +10,19 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('The question...'),
+          SizedBox(height: 30),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 1')),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 2')),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 3')),
+          ElevatedButton(onPressed: () {}, child: Text('Answer 4')),
+        ],
+      ),
+    );
   }
 }
