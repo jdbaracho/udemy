857f1505170d8521e13e22296ec843d103cf5c06
jdbaracho <joao.d.baracho@gmail.com>
2026-03-27 12:08:46 +0000

    [shopping_list] forms

---
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  final List<GroceryItem> _groceryItems = [];
+
+  void _addItem() async {
+    final newItem = await Navigator.of(
+      context,
+    ).push<GroceryItem>(MaterialPageRoute(builder: (context) => NewItem()));
+
+    if (newItem == null) {
+      return;
+    }
+
+    setState(() {
+      _groceryItems.add(newItem);
+    });
+  }
+
+  void _removeItem(GroceryItem item) {
+    setState(() {
+      _groceryItems.remove(item);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Groceries'),
+        actions: [IconButton(onPressed: _addItem, icon: Icon(Icons.add))],
+      ),
+      body: _groceryItems.isNotEmpty
+          ? ListView.builder(
+              itemCount: _groceryItems.length,
+              itemBuilder: (context, index) => Dismissible(
+                key: ValueKey(_groceryItems[index].id),
+                onDismissed: (direction) {
+                  _removeItem(_groceryItems[index]);
+                },
+                child: ListTile(
+                  leading: Container(
+                    color: _groceryItems[index].category.color,
+                    width: 24.0,
+                    height: 24.0,
+                  ),
+                  title: Text(_groceryItems[index].name),
+                  trailing: Text(_groceryItems[index].quantity.toString()),
+                ),
+              ),
+            )
+          : Center(child: Text('No items added.')),
+    );
+  }
+}
