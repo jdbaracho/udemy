5c2671fe379f99e2cc9c7457d479e8e5c623e553
jdbaracho <joao.d.baracho@gmail.com>
2026-04-13 15:34:57 +0100

    [weather_app] template

---
+++ swift/ios_development_kickstart/WeatherApp/WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by João Domingos Baracho on 13/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
